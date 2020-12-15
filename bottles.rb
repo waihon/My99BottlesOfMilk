00bf1cfe1d32485bad0532acc955f1bcd07230a7
Waihon Yew <yewwaihon@gmail.com>
2020-12-15 21:45:48 +0800

    Call bottles from remaining_bottles

--- bottles.rb
+++ bottles.rb
@@ -3,14 +3,15 @@ class Bottles
     verses(99, -3)
   end
 
-  def verses(hi, lo)
-    lo = 0 if lo < 0
-    hi.downto(lo).map {|n| verse(n) }.join("\n")
+  def verses(high, low)
+    @max = high
+    low = 0 if low < 0
+    high.downto(low).map {|n| verse(n) }.join("\n")
   end
 
   def verse(n)
-      initial_bottles(n) + 
-      remaining_bottles(n)
+    initial_bottles(n) + 
+    remaining_bottles(n)
   end
 
   def initial_bottles(n)
@@ -29,19 +30,15 @@ class Bottles
   end
 
   def remaining_bottles(n)
-    if n >= 3
-        "Take one down and pass it around, #{n - 1} bottles of milk on the wall.\n"
-    elsif n == 2
-        "Take one down and pass it around, #{n - 1} bottle of milk on the wall.\n"        
-    elsif n == 1
-        "Take one down and pass it around, no more bottles of milk on the wall.\n"       
+    if n > 0
+      "Take one down and pass it around, #{bottles(n-1)} on the wall.\n"
     else
-        "Go to the store and buy some more, 99 bottles of milk on the wall.\n"
+      "Go to the store and buy some more, #{bottles(@max)} on the wall.\n"
     end
   end
 end
 
 if __FILE__ == $0
-    bottles = Bottles.new
-    puts bottles.song
+  bottles = Bottles.new
+  puts bottles.song
 end
