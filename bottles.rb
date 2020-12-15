5c28f31acd588b76becdfdfb8a819f640518b9b7
Waihon Yew <yewwaihon@gmail.com>
2020-12-15 21:12:41 +0800

    Introduce initial_bottles and remaining_bottles

--- bottles.rb
+++ bottles.rb
@@ -7,20 +7,33 @@ class Bottles
     lo = 0 if lo < 0
     hi.downto(lo).map {|n| verse(n) }.join("\n")
   end
-
+  
   def verse(n)
+      initial_bottles(n) + 
+      remaining_bottles(n)
+  end
+  
+  def initial_bottles(n)
     if n >= 3
-        "#{n} bottles of milk on the wall, #{n} bottles of milk.\n" +
+        "#{n} bottles of milk on the wall, #{n} bottles of milk.\n"
+    elsif n == 2
+        "#{n} bottles of milk on the wall, #{n} bottles of milk.\n"
+    elsif n == 1
+        "#{n} bottle of milk on the wall, #{n} bottle of milk.\n"
+    else
+        "No more bottles of milk on the wall, no more bottles of milk.\n" 
+    end
+  end
+  
+  def remaining_bottles(n)
+    if n >= 3
         "Take one down and pass it around, #{n - 1} bottles of milk on the wall.\n"
     elsif n == 2
-        "#{n} bottles of milk on the wall, #{n} bottles of milk.\n" +
         "Take one down and pass it around, #{n - 1} bottle of milk on the wall.\n"        
     elsif n == 1
-        "#{n} bottle of milk on the wall, #{n} bottle of milk.\n" +
         "Take one down and pass it around, no more bottles of milk on the wall.\n"       
     else
-        "No more bottles of milk on the wall, no more bottles of milk.\n" + 
-        "Go to the store and buy some more, 99 bottles of milk on the wall."
+        "Go to the store and buy some more, 99 bottles of milk on the wall.\n"
     end
   end
 end
